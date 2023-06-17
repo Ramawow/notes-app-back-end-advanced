6f988d09ac64875fbfd845a4caaefc3efd470629
ramawow <rmt0276@gmail.com>
2023-06-17 09:25:31 +0700

    authentication implemented

--- src/services/postgres/UsersService.js
+++ src/services/postgres/UsersService.js
@@ -3,7 +3,7 @@ const { nanoid } = require('nanoid');
 const bcrypt = require('bcrypt');
 const InvariantError = require('../../exceptions/InvariantError');
 const NotFoundError = require('../../exceptions/NotFoundError');
-// const AuthenticationError = require('../../exceptions/AuthenticationError');
+const AuthenticationError = require('../../exceptions/AuthenticationError');
 
 class UsersService {
     constructor() {
@@ -53,25 +53,25 @@ class UsersService {
         return result.rows[0];
     }
 
-    // async verifyUserCredential(username, password) {
-    //     const query = {
-    //         text: 'SELECT id, password FROM users WHERE username = $1',
-    //         values: [username],
-    //     };
-    //     const result = await this._pool.query(query);
+    async verifyUserCredential(username, password) {
+        const query = {
+            text: 'SELECT id, password FROM users WHERE username = $1',
+            values: [username],
+        };
+        const result = await this._pool.query(query);
 
-    //     if (!result.rows.length) {
-    //         throw new AuthenticationError('Kredensial yang Anda berikan salah');
-    //     }
-    //     const { id, password: hashedPassword } = result.rows[0];
+        if (!result.rows.length) {
+            throw new AuthenticationError('Kredensial yang Anda berikan salah');
+        }
+        const { id, password: hashedPassword } = result.rows[0];
 
-    //     const match = await bcrypt.compare(password, hashedPassword);
+        const match = await bcrypt.compare(password, hashedPassword);
 
-    //     if (!match) {
-    //         throw new AuthenticationError('Kredensial yang Anda berikan salah');
-    //     }
-    //     return id;
-    // }
+        if (!match) {
+            throw new AuthenticationError('Kredensial yang Anda berikan salah');
+        }
+        return id;
+    }
 
     async getUserByUsername(username) {
         const query = {
